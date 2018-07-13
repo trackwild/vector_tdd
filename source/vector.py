08962a0dfc0af0760364ccb87f3f55bc26f24e41
Simon <Rene.Simon@McKesson.com>
2018-07-13 09:17:25 -0500

    added test methods and corresponding code

--- source/vector.py
+++ source/vector.py
@@ -2,5 +2,9 @@ class Vector:
     
     def __init__(self, nums):
         self.nums = nums
+        self.dims = len(nums)
+        self.norm = self.length()
 
+    def length(self):
+        return sum([c**2 for c in self.nums])**0.5
 
--- test/test_vector.py
+++ test/test_vector.py
@@ -3,3 +3,12 @@ from source.vector import Vector
 def test_make_vector():
     v = Vector([1, 2, 3])
     assert len(v.nums) == 3
+
+def test_dimension():
+    v = Vector([2,4,6,8])
+    assert v.dims == 4
+
+def test_norm():
+    v = Vector([3,4])
+    assert v.norm == 5
+    
